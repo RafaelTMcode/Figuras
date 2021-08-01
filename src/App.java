ee001c87149b74334ca54a2e855f472c50ec0204
Rafa TM <rafael.tomas-martinez@capgemini.com>
2021-08-01 23:30:24 +0200

    Prueba borrar comentarios

--- src/App.java
+++ src/App.java
@@ -12,39 +12,6 @@ public class App {
         for(int i=0; i< lienzo.lista.size();i++){
             System.out.println(lienzo.lista.get(i));
         }
-        
 
-        //System.out.println("Bienvenido a la fabrica de figuras");
-        //System.out.println("¿Que figura quieres fabricar?");
-
-        //Elige una de las tres figuras posibles
-
-        //System.out.println("¿De que color quieres el borde de la figura?");
-        //mandamos el valor a draw
-
-        //System.out.println("¿De que color quieres el fondo de la figura?");
-        //mandamos el valor a draw
-
-        //
-        // llamamos a los metodos necesarios
-        //
-
-        //ya tenemos el objeto creado
-
-
-
-
-        // sacamos por pantalla que figura se ha fabricado
-        //System.out.println("¿Que figura quieres fabricar?");
-
-
-
-/*         if(enumFigura==Figura.RAYO){
-            return new Rayo();
-        }
-        else if(figuras==Figura.CORAZON){
-            return new corazon;
-        }
-            return new estrella; */
     }
 }
